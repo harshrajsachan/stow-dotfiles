102112ea135c81e50d7f78b5824a9ee8498ca284
harshraj.sachan17@gmail.com <harshraj.sachan17@gmail.com>
2026-07-15 23:57:30 +0530

    safe

--- i3/.config/i3/scripts/wall_toggle.sh
+++ i3/.config/i3/scripts/wall_toggle.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
 WALL_DIR="$HOME/Pictures"
+STATE="$HOME/.cache/wall_index"
+DEFAULT="201.jpg"
 
-# Only images directly inside ~/Pictures
 mapfile -t WALLPAPERS < <(
   find "$WALL_DIR" -maxdepth 1 -type f \
     \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) | sort
 )
 
 COUNT=${#WALLPAPERS[@]}
-[[ $COUNT -eq 0 ]] && exit 1
+((COUNT == 0)) && exit 1
 
-STATE="$HOME/.cache/wall_index"
+# Find the index of the default wallpaper
+DEFAULT_INDEX=0
+for i in "${!WALLPAPERS[@]}"; do
+  if [[ "$(basename "${WALLPAPERS[$i]}")" == "$DEFAULT" ]]; then
+    DEFAULT_INDEX=$i
+    break
+  fi
+done
 
+# Startup mode
+if [[ "$1" == "default" ]]; then
+  feh --bg-fill "${WALLPAPERS[$DEFAULT_INDEX]}"
+  echo $(((DEFAULT_INDEX + 1) % COUNT)) >"$STATE"
+  exit 0
+fi
+
+# Cycle mode
 if [[ -f "$STATE" ]]; then
   INDEX=$(<"$STATE")
 else
-  INDEX=0
+  INDEX=$DEFAULT_INDEX
 fi
 
 INDEX=$((INDEX % COUNT))
